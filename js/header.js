de325a77347b77ef18f840b83b7e2efad16a58c4
gauricc <48559138+gauricc@users.noreply.github.com>
2019-06-30 13:58:19 +0530

    Update header.js

---
+++ js/header.js
@@ -0,0 +1,15 @@
+function hideSignInModal(){
+var modal =document.getElementById("signin-modal-id");
+}
+
+function displaySignInModal(){
+var modal=document.getElementById("signin-modal-id");
+}
+
+function hideSignUpModal(){
+var modal = document.getElementById("signup-modal-id");
+}
+
+functiopn displaySignUpModal(){
+var modal = document.getElementById("signup-modal-id");
+}
